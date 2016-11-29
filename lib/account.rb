19f41c5136d82cc4b3f53eca46e6a297eb87f12f
MaxProvin <maxprovin01@gmail.com>
2016-11-29 14:39:47 +0000

    transactions are logged

--- lib/account.rb
+++ lib/account.rb
@@ -2,19 +2,30 @@ class Account
 
 	INITIAL_BALANCE = 0
 
-	attr_reader :owner, :balance
+	attr_reader :owner, :balance, :transactions
 
 	def initialize(owner)
 		@owner = owner
 		@balance = INITIAL_BALANCE
+		@transactions = []
 	end
 
 	def deposit(amount)
 		@balance += amount
+		transactions.push({
+			date: DateTime.now,
+			transaction: amount,
+			balance: balance
+			})
 	end
 
 	def withdraw(amount)
 		@balance -= amount
+		transactions.push({
+			date: DateTime.now,
+			transaction: -amount,
+			balance: balance
+			})
 	end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,15 @@ require 'account'
 describe Account do
 	
 	subject(:account) { described_class.new('Max') }
+	let(:sample_transaction) {[{
+		date: DateTime.new(2016, 1, 15),
+		transaction: 100,
+		balance: 100
+		}, {
+		date: DateTime.new(2016, 1, 15),
+		transaction: -50,
+		balance: 50			
+		}]}
 
 	it 'Has an owner' do
 		expect(account.owner).to eq 'Max'
@@ -12,8 +21,9 @@ describe Account do
 		expect(account.balance).to eq 0
 	end
 
-	context 'Transaction: ' do
+	context 'Transactions: ' do
 		before do
+			allow(DateTime).to receive(:now) { DateTime.new(2016, 1, 15) }
 			account.deposit(100)
 		end
 
@@ -30,6 +40,18 @@ describe Account do
 		it 'Withdrawal is succesful' do
 			account.withdraw(50)
 			expect(account.balance).to eq 50
+		end
+
+		it 'Are Logged with date, transaction and balance' do
+			account.withdraw(50)
+			expect(account.transactions).to eq sample_transaction
+		end
+	end
+
+	context 'Statement: ' do
+		xit 'Each transaction has the date it was made' do
+			account.deposit(100)
+			expect(account.transactions['date'])
 		end
 	end
 
